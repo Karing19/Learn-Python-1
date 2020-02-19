241a8aa67d3577857150c6fd4b03efc974d90c22
Chenyang Yu <ch3ny4n6@gmail.com>
2020-02-18 18:27:27 -0800

    Add files via upload

---
+++ chapter_3/3-10-every_function.py
@@ -0,0 +1,9 @@
+categories = ['Pwn', 'Reverse', 'Web', 'Crypto', 'Misc']
+categories.sort()
+print(categories)
+categories.sort(reverse=True)
+print(categories)
+print(sorted(categories))
+categories.reverse()
+print(categories)
+print(len(categories))
