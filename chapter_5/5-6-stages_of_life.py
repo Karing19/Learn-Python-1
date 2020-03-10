f209b0e3daaee00de401b6e429339b7ff6e2c9bd
Chenyang Yu <ch3ny4n6@gmail.com>
2020-03-09 17:20:57 -0700

    Update 5-6-stages_of_life.py

--- chapter_5/5-6-stages_of_life.py
+++ chapter_5/5-6-stages_of_life.py
@@ -2,13 +2,13 @@ age = 23
 
 if age < 2:
 	print("You are a baby.")
-elif 2 <= age <= 4:
+elif 2 <= age < 4:
 	print("You are a toddler.")
-elif 4 <= age <= 13:
+elif 4 <= age < 13:
 	print("You are a toddler.")
-elif 13 <= age <= 20:
+elif 13 <= age < 20:
 	print("You are a teenager.")
-elif 20 <= age <= 65:
+elif 20 <= age < 65:
 	print("You are an adult.")
-elif age > 65:
+elif age >= 65:
 	print("You are an elder.")
