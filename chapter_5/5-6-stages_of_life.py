3259824e5cc4fc6eb14ef22df0bf37df41bfc2ee
Chenyang Yu <ch3ny4n6@gmail.com>
2020-03-09 17:36:45 -0700

    Update 5-6-stages_of_life.py

--- chapter_5/5-6-stages_of_life.py
+++ chapter_5/5-6-stages_of_life.py
@@ -5,7 +5,7 @@ if age < 2:
 elif 2 <= age < 4:
 	print("You are a toddler.")
 elif 4 <= age < 13:
-	print("You are a toddler.")
+	print("You are a kid.")
 elif 13 <= age < 20:
 	print("You are a teenager.")
 elif 20 <= age < 65:
